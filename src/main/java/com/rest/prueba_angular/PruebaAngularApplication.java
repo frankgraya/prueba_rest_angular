b5c864aa888c83ecce60df05ea59a4c1c318e1cc
Francisco Granados <francisco.graya26@gmail.com>
2023-01-23 11:25:11 -0600

    subiendo a git

---
+++ src/main/java/com/rest/prueba_angular/PruebaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.rest.prueba_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/prueba_angular/PruebaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.prueba_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
